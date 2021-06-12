7d04064b2b153d5014379aa4541034dfa9f183e5
Leopoldov95 <leo386_ortega@mail.fresnostate.edu>
2021-06-12 07:11:38 -0500

    tutorial sucks

---
+++ backend/api/restaurants.controller.js
@@ -0,0 +1,34 @@
+import RestaurantDAO from "./dao/restaurantsDAO.js";
+
+export default class RestaurantsController {
+  static async apiGetRestaurants(req, res, next) {
+    const restaurantsPerPage = req.query.restaurantsPerPage
+      ? parseInt(parseInt(req.query.restaurantsPerPag, 10))
+      : 20;
+    const page = req.query.page ? parseInt(req.query.page, 10) : 0;
+
+    let filters = {};
+    if (req.query.cuisine) {
+      filters.cuisine = req.query.cuisine;
+    } else if (req.query.zipcode) {
+      filters.zipcode = req.query.zipcode;
+    } else if (req.query.name) {
+      filters.name = req.query.name;
+    }
+
+    const { restaurantsList, totalNumRestaurants } =
+      await RestaurantDAO.getRestaurants({
+        filters,
+        page,
+        restaurantsPerPage,
+      });
+    let response = {
+      restaurants: restaurantsList,
+      page: page,
+      filters: filters,
+      entries_per_page: restaurantsPerPage,
+      total_results: totalNumRestaurants,
+    };
+    res.json(response);
+  }
+}
