e88da6614b59344456773ce2e8a57985cf5d8f42
Leopoldov95 <leo386_ortega@mail.fresnostate.edu>
2021-06-20 15:46:43 -0500

    server side files

--- backend/api/restaurants.route.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import RestaurantsCtrl from "./restaurants.controller.js";
-const router = express.Router();
-
-router.route("/").get(RestaurantsCtrl.apiGetRestaurants);
-
-export default router;
