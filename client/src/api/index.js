a623c05d2b1eeac0d6c0144bb322526bd7bf5757
Leopoldov95 <leo386_ortega@mail.fresnostate.edu>
2022-09-03 21:01:14 -0500

    updated API url

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // so by using this url, we can use the backend logic
 const API = axios.create({
-  baseURL: "https://bicycleshop-api.herokuapp.com"
+  baseURL: "https://bicycle-ecomm-api.onrender.com/",
 });
 // this function will run on every request, it's helping the middleware function
 API.interceptors.request.use((req) => {
